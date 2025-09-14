596086533f0b8f1df25085d1ab5e783a4836c705
rougemeilland <28302784+rougemeilland@users.noreply.github.com>
2025-09-14 20:14:53 +0900

    20250914

---
+++ Experiment.OptimizingNumericalCalculations.CUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using Experiment.OptimizingNumericalCalculations.ClassLibrary;
+namespace Experiment.OptimizingNumericalCalculations.CUI
+{
+    internal sealed class Program
+    {
+        private static void Main()
+        {
+            _ = Sample.Negatve(1);
+            _ = Sample.Negatve(1d);
+            _ = Sample.Negatve(Complex.One);
+            _ = Sample.Difference(1, -1);
+            _ = Sample.Difference(1d, -1d);
+            _ = Sample.Difference(Complex.One, Complex.ImaginaryOne);
+        }
+    }
+}
---
+++ Experiment.OptimizingNumericalCalculations.ClassLibrary/Sample.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Experiment.OptimizingNumericalCalculations.ClassLibrary
+{
+    public static class Sample
+    {
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.AggressiveOptimization)]
+        public static int Negatve(int x) => -x; // equivalent to "-x"
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.AggressiveOptimization)]
+        public static double Negatve(double x) => -x; // equivalent to "x ^ 0x80000000"
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.AggressiveOptimization)]
+        public static Complex Negatve(Complex x) => -x; // equivalent to "new Complex(x.Real ^ 0x80000000, x.Imaginary ^ 0x80000000)"
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.AggressiveOptimization)]
+        public static int Difference(int x, int y) => -x + y; // equivalent to "y - x"
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.AggressiveOptimization)]
+        public static double Difference(double x, double y) => -x + y; // equivalent to "y - x"
+
+        // equivalent to "new Complex(x.Real ^ 0x80000000 + y, x.Imaginary ^ 0x80000000 + y)"
+        // Although this calculation is equivalent to "new Complex(y - x, y - x)", it requires more computational cost.
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.AggressiveOptimization)]
+        public static Complex Difference(Complex x, Complex y) => -x + y;
+    }
+}
